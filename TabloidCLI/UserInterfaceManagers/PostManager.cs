8602c09690181e83a25d3660963bff5329cd791f
DavidEdwardDarden <davidedarden83@gmail.com>
2021-06-15 12:08:59 -0500

    completed second ticket

---
+++ TabloidCLI/UserInterfaceManagers/PostManager.cs
@@ -0,0 +1,63 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace TabloidCLI.UserInterfaceManagers
+{
+    class PostManager : IUserInterfaceManager
+    {
+
+        private readonly IUserInterfaceManager _parentUI;
+
+        public PostManager(IUserInterfaceManager parentUI, string connectionString)
+        {
+            _parentUI = parentUI;
+        }
+
+
+        public IUserInterfaceManager Execute()
+        {
+            Console.WriteLine("Post Menu");
+            Console.WriteLine("1) List Posts");
+            Console.WriteLine("2) Add Post");
+            Console.WriteLine("3) Edit Post");
+            Console.WriteLine("4) Remove Post");
+            Console.WriteLine("5) Note Management");
+            Console.WriteLine("0) Return to Main Menu");
+
+            Console.Write("> ");
+            string choice = Console.ReadLine();
+            switch (choice)
+            {
+                case "1":
+                    throw new NotImplementedException();
+                    break;
+                case "2":
+                    throw new NotImplementedException();
+                    break;
+                case "3":
+                    throw new NotImplementedException();
+                    break;
+                case "4":
+                    throw new NotImplementedException();
+                    break;
+                case "5":
+                    throw new NotImplementedException();
+                    break;
+                case "0":
+                    return _parentUI;
+                default:
+                    Console.WriteLine("Invalid Selection");
+                    break;
+            }
+
+            throw new NotImplementedException();
+        }
+
+
+
+
+
+
+    }
+}
